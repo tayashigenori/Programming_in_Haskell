4908d4c87b045cef6f042bf258dfe3cc2cfc20a8
Shigenori Taya <shigenori.taya@gmail.com>
2015-02-15 20:42:53 +0900

    adding 09 dentaku

--- 09_interactive_program/dentaku.hs
+++ 09_interactive_program/dentaku.hs
@@ -1,3 +1,4 @@
+import Parser
 import System.IO
 
 -- 9.2. 入出力の型
@@ -56,7 +57,7 @@ beep :: IO()
 beep = putStr "\BEL"
 
 cls :: IO()
-cls = putStr "ESC[2J"
+cls = putStr "\ESC[2J"
 
 type Pos = (Int, Int)
 
@@ -75,4 +76,72 @@ seqn (a: as) = do a
 
 --putStr xs = seqn [putChar x | x <- xs]
 
+--9.6. 電卓
+box :: [String]
+box = ["+---------------+",
+       "|               |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | - |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
 
+buttons :: [Char]
+buttons = standard ++ extra
+          where
+            standard = "qcd=123+456-789*0()/"
+            extra    = "QCD \ESC\BS\DEL\n"
+
+showbox :: IO()
+showbox = seqn [writeat (1, y) xs | (y, xs) <- zip [1..13] box]
+
+display    :: String -> IO()
+display xs = do writeat (3, 2) "                   "
+                writeat (3, 2) (reverse (take 13 (reverse xs)))
+
+calc    :: String -> IO()
+calc xs = do display xs
+             c <- getCh
+             if elem c buttons then
+                 process c xs
+             else
+                 do beep
+                    calc xs
+
+process                :: Char -> String -> IO()
+process c xs
+  | elem c "qQ\ESC"    = quit
+  | elem c "dD\BS\DEL" = delete xs
+  | elem c "=\n"       = eval xs
+  | elem c "cC"        = clear
+  | otherwise          = press c xs
+
+quit :: IO()
+quit = goto (1, 14)
+
+delete    :: String -> IO()
+delete "" = calc ""
+delete xs = calc (init xs)
+
+eval    :: String -> IO()
+eval xs = case parse expr xs of
+            [(n, "")] -> calc (show n)
+            _ -> do beep
+                    calc xs
+
+clear :: IO()
+clear = calc ""
+
+press      :: Char -> String -> IO()
+press c xs = calc (xs ++ [c])
+
+run :: IO()
+run = do cls
+         showbox
+         clear
